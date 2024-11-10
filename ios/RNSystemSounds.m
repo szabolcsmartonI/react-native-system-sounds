5b0dabb5bbdb3af7ad5456b63713a24c80249038
szabolcsmartonI <98264364+szabolcsmartonI@users.noreply.github.com>
2024-11-10 02:54:27 +0100

    Update RNSystemSounds.m

--- ios/RNSystemSounds.m
+++ ios/RNSystemSounds.m
@@ -1,12 +1,47 @@
+// RNSystemSounds.m
+
 #import "RNSystemSounds.h"
 #import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation RNSystemSounds
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(playSystemSound: (nonnull NSInteger *) soundID){
-    AudioServicesPlaySystemSound (soundID);
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self configureAudioSession];
+    }
+    return self;
+}
+
+- (void)configureAudioSession {
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    NSError *error = nil;
+    
+    // Az audio session kategória beállítása
+    [audioSession setCategory:AVAudioSessionCategoryPlayback
+                  withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker
+                        error:&error];
+    
+    if (error) {
+        NSLog(@"Error setting audio session category: %@", error);
+        return;
+    }
+    
+    // Audio session aktiválása
+    [audioSession setActive:YES error:&error];
+    
+    if (error) {
+        NSLog(@"Error activating audio session: %@", error);
+        return;
+    }
+}
+
+RCT_EXPORT_METHOD(playSystemSound:(NSInteger)soundID) {
+    // Rendszerhang lejátszása
+    AudioServicesPlaySystemSound(soundID);
 }
 
 @end
